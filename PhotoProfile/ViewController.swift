53dd61c13c5edcb65fe61db165c820db9214c2ef
saranghe41 <saranghe-0-@hanmail.net>
2021-11-11 23:39:51 +0900

    프로필 사진 변경 개발중

--- PhotoProfile/ViewController.swift
+++ PhotoProfile/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileChangeBtn: UIButton!
+    @IBOutlet weak var profileImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("ViewController - viewDidLoad() called")
+        
+        profileImg.layer.cornerRadius = profileImg.frame.height / 2
+        profileChangeBtn.layer.cornerRadius = 10
+        
+        profileChangeBtn.addTarget(self, action: #selector(onProfileChangeBtnClicked), for: .touchUpInside)
     }
 
+    // 프사 변경 버튼이 클릭되었을때
+    @objc fileprivate func onProfileChangeBtnClicked() {
+        print("ViewController - onProfileChangeBtnClicked() called")
+        
+        // 카메라 라이브러리 셋팅
+        var config = YPImagePickerConfiguration()
+//        config.screens = [.library, .photo, .video]
+        config.screens = [.library]
 
+        let picker = YPImagePicker(configuration: config)
+        
+        // 사진이 선택되었을때
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto { 
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                
+                self.profileImg.image = photo.image
+            }
+            // 사진 선택창 닫기
+            picker.dismiss(animated: true, completion: nil)
+        }
+        // 사진 선택창 보여주기
+        present(picker, animated: true, completion: nil)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PhotoProfile' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # 사진라이브러리
+  pod 'YPImagePicker'
+  use_frameworks!
+
+  # Pods for PhotoProfile
+
+end
